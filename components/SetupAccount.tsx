334e8074a748a2b6337fa8c3cf30e237eadd0113
Rakib <rakib@think-engineer.com>
2024-05-22 00:47:08 +0100

    join org when setting up new acc

--- components/SetupAccount.tsx
+++ components/SetupAccount.tsx
@@ -3,7 +3,16 @@ import {
   db,
   // storage
 } from "@/firebase";
-import { addDoc, collection, doc, setDoc, updateDoc } from "firebase/firestore";
+import { generatePassword } from "@/lib/helperFunctions";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import React, { useContext, useRef, useState } from "react";
 
@@ -12,39 +21,96 @@ export default function SetupAccount() {
 
   const [name, setName] = useState("");
   const [orgName, setOrgName] = useState("");
+  const [passcode, setPasscode] = useState("");
+
+  const [error, setError] = useState<any>("");
 
   const [mode, setMode] = useState<"join-org" | "create-org">("create-org");
 
   const finishProfileSetup = async () => {
+    setError("");
+
     const firestoreUser = {
       name,
       email: user.email,
     };
 
-    const newOrg = {
-      name: orgName,
-      createdAt: new Date(),
-      createdBy: {
-        userId: user.uid,
-        name,
-      },
-      users: [
-        {
+    if (mode === "create-org") {
+      const newOrg = {
+        name: orgName,
+        passcode: generatePassword(8),
+        createdAt: new Date(),
+        createdBy: {
+          userId: user.uid,
+          name,
+        },
+        users: [
+          {
+            id: user.uid,
+            ...firestoreUser,
+          },
+        ],
+      };
+      let orgDoc = await addDoc(collection(db, "organisations"), newOrg);
+
+      let userDoc = await setDoc(doc(db, "users", user.uid), {
+        ...firestoreUser,
+        organisation: {
+          id: orgDoc.id,
+          name: orgName,
+        },
+      });
+
+      let finalUser = {
+        ...user,
+        ...firestoreUser,
+        organisation: {
+          id: orgDoc.id,
+          name: orgName,
+        },
+        setup: true,
+      };
+
+      setUser(finalUser);
+    } else if (mode === "join-org") {
+      // CHECK IF ORG EXISTS & PASSCODE IS ACCURATE
+      let snapshot = await getDocs(collection(db, "organisations"));
+      let orgDoc: any = null;
+      snapshot.forEach((doc) => {
+        if (doc.data().name === orgName) {
+          orgDoc = { id: doc.id, ...doc.data() };
+        }
+      });
+      if (!orgDoc) return setError("Organisation does not exist");
+      if (orgDoc?.passcode !== passcode) return setError("Incorrect Passcode");
+
+      // EXISTS AND PASSCODE CORRECT
+      let userDoc = await setDoc(doc(db, "users", user.uid), {
+        ...firestoreUser,
+        organisation: {
+          id: orgDoc.id,
+          name: orgName,
+        },
+      });
+      await updateDoc(doc(db, "organisations", orgDoc.id), {
+        users: arrayUnion({
           id: user.uid,
           ...firestoreUser,
-        },
-      ],
-    };
-    let orgDoc = await addDoc(collection(db, "organisations"), newOrg);
+        }),
+      });
 
-    let userDoc = await setDoc(doc(db, "users", user.uid), {
-      ...firestoreUser,
-      organisation: {
-        id: orgDoc.id,
-        name: orgName,
-      },
-    });
-    setUser({ ...user, ...firestoreUser, setup: true });
+      let finalUser = {
+        ...user,
+        ...firestoreUser,
+        organisation: {
+          id: orgDoc.id,
+          name: orgName,
+        },
+        setup: true,
+      };
+
+      setUser(finalUser);
+    }
   };
 
   return (
@@ -61,22 +127,57 @@ export default function SetupAccount() {
             placeholder="name"
           />
         </div>
-        <div className="flex flex-col gap-1">
-          <label className="">Organisation Name </label>
-          <input
-            type="text"
-            className="border outline-none p-2"
-            value={orgName}
-            onChange={(e) => setOrgName(e.target.value)}
-            placeholder="Organisation Name"
-          />
-        </div>
-        <span
-          className="cursor-pointer underline text-blue-500 text-sm"
-          onClick={() => setMode("join-org")}
-        >
-          Join existing Organisation
-        </span>
+
+        {mode === "create-org" ? (
+          <>
+            <div className="flex flex-col gap-1">
+              <label className="">Organisation Name </label>
+              <input
+                type="text"
+                className="border outline-none p-2"
+                value={orgName}
+                onChange={(e) => setOrgName(e.target.value)}
+                placeholder="Organisation Name"
+              />
+            </div>
+
+            <span
+              className="cursor-pointer underline text-blue-500 text-sm"
+              onClick={() => setMode("join-org")}
+            >
+              Join existing Organisation
+            </span>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col gap-1">
+              <label className="">Organisation Name </label>
+              <input
+                type="text"
+                className="border outline-none p-2"
+                value={orgName}
+                onChange={(e) => setOrgName(e.target.value)}
+                placeholder="Organisation Name"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label className="">Organisation Passcode </label>
+              <input
+                type="text"
+                className="border outline-none p-2"
+                value={passcode}
+                onChange={(e) => setPasscode(e.target.value)}
+                placeholder="Organisation Passcode"
+              />
+            </div>
+            <span
+              className="cursor-pointer underline text-blue-500 text-sm"
+              onClick={() => setMode("create-org")}
+            >
+              Create New Organisation
+            </span>
+          </>
+        )}
 
         <button
           className="btn btn-primary"
@@ -85,6 +186,9 @@ export default function SetupAccount() {
         >
           Finish Profile Set Up
         </button>
+        {error && (
+          <div className="p-2 bg-red-200 text-red-600 text-center">{error}</div>
+        )}
       </div>
     </div>
   );
