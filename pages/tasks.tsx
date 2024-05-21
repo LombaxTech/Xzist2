bfc0dd14a3577105bc48a644df3c2a917d14529d
Rakib <rakib@think-engineer.com>
2024-05-21 16:07:45 +0100

    tasks filter layout

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -1,4 +1,5 @@
 import CreateTaskModal from "@/components/CreateTaskModal";
+import EditTaskModal from "@/components/EditTaskModal";
 import SidebarPageLayout from "@/components/SidebarPageLayout";
 import { db } from "@/firebase";
 import { collection, onSnapshot } from "firebase/firestore";
@@ -29,16 +30,27 @@ export default function Tasks() {
         <CreateTaskModal />
       </div>
 
-      <div className="flex flex-col gap-2 mt-8">
-        {tasks.length === 0 ? "No Tasks" : ""}
-        {tasks &&
-          tasks.map((task: any, i: number) => {
-            return (
-              <div key={i} className="p-2 shadow-md">
-                {task.description}
-              </div>
-            );
-          })}
+      <div className="flex gap-4 mt-8">
+        {/* FILTERS */}
+        <div className="p-4 px-16 shadow-md flex flex-col gap-2">
+          <h2 className="text-xl font-medium">Filters</h2>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-2">
+          {tasks.length === 0 ? "No Tasks" : ""}
+          {tasks &&
+            tasks.map((task: any, i: number) => {
+              return (
+                <div
+                  key={i}
+                  className="p-2 shadow-md flex items-center justify-between"
+                >
+                  <span className="">{task.description}</span>
+                  <EditTaskModal task={task} />
+                </div>
+              );
+            })}
+        </div>
       </div>
     </SidebarPageLayout>
   );
